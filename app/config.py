ede2fbc4202055c0a2aee5d24669751205813622
Dominik Sawicki <dominik.sawicki52@gmail.com>
2021-11-29 23:51:59 +0100

    Finished JWT authentication

--- app/config.py
+++ app/config.py
@@ -7,6 +7,7 @@ from pydantic import BaseSettings, Field
 
 class Settings(BaseSettings):
     db_url: str = Field(..., env="DATABASE_URL")
+    token_secret: str = Field(..., env="JWT_SECRET")
 
 
 settings = Settings()
