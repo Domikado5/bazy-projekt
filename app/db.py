b0fd10f9224953664a880a9298c0f7e543cbc48b
D3nz13 <seba.szczepan2@gmail.com>
2021-12-02 12:57:58 +0100

    Created all models

--- app/db.py
+++ app/db.py
@@ -1,9 +1,10 @@
-from typing import ForwardRef
+from typing import ForwardRef, Optional
 import databases
 import ormar
 import sqlalchemy
 import datetime
 import decimal
+from pydantic import BaseModel
 
 from .config import settings
 
@@ -14,6 +15,11 @@ metadata = sqlalchemy.MetaData()
 class BaseMeta(ormar.ModelMeta):
     metadata = metadata
     database = database
+
+
+class AuthCredentials(BaseModel):
+    username: str
+    password: str
 
 
 class User(ormar.Model):
@@ -32,6 +38,13 @@ class User(ormar.Model):
     )
 
 
+class UserUpdate(BaseModel):
+    username: Optional[str] = None
+    password: Optional[str] = None
+    email: Optional[str] = None
+    role: Optional[str] = None
+
+
 class Post(ormar.Model):
     class Meta(BaseMeta):
         tablename = "posts"
@@ -43,6 +56,12 @@ class Post(ormar.Model):
         nullable=False, default=datetime.datetime.now()
     )
     author: User = ormar.ForeignKey(User)
+
+
+class PostUpdate(BaseModel):
+    title: Optional[str] = None
+    content: Optional[str] = None
+    date: Optional[datetime.datetime] = None
 
 
 class Comment(ormar.Model):
@@ -64,6 +83,31 @@ class Unit(ormar.Model):
 
     id: int = ormar.Integer(primary_key=True)
     unitname: str = ormar.String(max_length=32, nullable=False, unique=True)
+
+
+class Allergen(ormar.Model):
+    class Meta(BaseMeta):
+        tablename = "allergens"
+
+    id: int = ormar.Integer(primary_key=True)
+    allergen: str = ormar.String(max_length=128, nullable=False)
+
+
+ProductCategoryRef = ForwardRef("ProductCategory")
+
+
+class ProductCategory(ormar.Model):
+    class Meta(BaseMeta):
+        tablename = "product_categories"
+
+    id: int = ormar.Integer(primary_key=True)
+    category_name: str = ormar.String(max_length=128, nullable=False)
+    root_category: ProductCategoryRef = ormar.ForeignKey(
+        ProductCategoryRef, related_name="root_categories"
+    )
+
+
+ProductCategory.update_forward_refs()
 
 
 class Product(ormar.Model):
@@ -94,23 +138,78 @@ class Product(ormar.Model):
         default="not verified",
     )
     unit: Unit = ormar.ForeignKey(Unit)
-
-
-ProductCategoryRef = ForwardRef("ProductCategory")
-
-
-class ProductCategory(ormar.Model):
-    class Meta(BaseMeta):
-        tablename = "product_categories"
-
-    id: int = ormar.Integer(primary_key=True)
-    category_name: str = ormar.String(max_length=128, nullable=False)
-    root_category: ProductCategoryRef = ormar.ForeignKey(
-        ProductCategoryRef, related_name="root_categories"
+    categories: ProductCategory = ormar.ForeignKey(ProductCategory)
+    allegens: Allergen = ormar.ManyToMany(
+        Allergen,
+        through_relation_name="product_id",
+        through_reverse_relation_name="allergen_id",
     )
 
 
-ProductCategory.update_forward_refs()
+class Entry(ormar.Model):
+    class Meta(BaseMeta):
+        tablename = "entries"
+
+    id: int = ormar.Integer(primary_key=True)
+    amount: decimal.Decimal = ormar.Decimal(
+        max_digits=10, decimal_places=3, nullable=False
+    )
+
+
+class Diary(ormar.Model):
+    class Meta(BaseMeta):
+        tablename = "diaries"
+
+    id: int = ormar.Integer(primary_key=True)
+    date: datetime.date = ormar.Date(nullable=False, default=datetime.date.today())
+    total_calories: decimal.Decimal = ormar.Decimal(
+        max_digits=10, decimal_places=3, nullable=False
+    )
+    total_fats: decimal.Decimal = ormar.Decimal(
+        max_digits=10, decimal_places=3, nullable=False
+    )
+    total_proteins: decimal.Decimal = ormar.Decimal(
+        max_digits=10, decimal_places=3, nullable=False
+    )
+    total_carbohydrates: decimal.Decimal = ormar.Decimal(
+        max_digits=10, decimal_places=3, nullable=False
+    )
+    products: Product = ormar.ManyToMany(
+        Product,
+        through=Entry,
+        through_relation_name="diary_id",
+        through_reverse_relation_name="product_id",
+    )
+    owner: User = ormar.ForeignKey(User)
+
+
+class SetCategory(ormar.Model):
+    class Meta(BaseMeta):
+        tablename = "set_categories"
+
+    id: int = ormar.Integer(primary_key=True)
+    category_name: str = ormar.String(max_length=128, nullable=False)
+
+
+class Set(ormar.Model):
+    class Meta(BaseMeta):
+        tablename = "sets"
+
+    id: int = ormar.Integer(primary_key=True)
+    set_name: str = ormar.String(max_length=128, nullable=False)
+    description: str = ormar.Text(nullable=True)
+    owner: User = ormar.ForeignKey(User, nullable=False)
+    products: Product = ormar.ManyToMany(
+        Product,
+        through_relation_name="set_id",
+        through_reverse_relation_name="product_id",
+    )
+    categories: SetCategory = ormar.ManyToMany(
+        SetCategory,
+        through_relation_name="set_id",
+        through_reverse_relation_name="category_id",
+    )
+
 
 engine = sqlalchemy.create_engine(settings.db_url)
 metadata.create_all(engine)
